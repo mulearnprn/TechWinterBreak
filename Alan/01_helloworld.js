8375a5f440a270a785d1689c0bf156fda03ce837
Alan Reji <143480799+alanreji2004@users.noreply.github.com>
2025-01-02 11:08:33 +0530

    Create 01_helloworld.js

---
+++ Alan/01_helloworld.js
@@ -0,0 +1,9 @@
+// Question 1: Print "Hello, World!" to the console.
+
+
+function printHelloWorld() {
+    console.log("Hello, world")
+  }
+  
+  printHelloWorld(); 
+  // Expected Output: Hello, World!
